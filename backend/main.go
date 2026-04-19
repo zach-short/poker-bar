80dd61e80520f1bb15e2e55c3e98c8f0acab256c
zachmshort <zach.short@fantomworks.com>
2026-04-19 19:09:09 -0400

    added website as allowed origin

--- backend/main.go
+++ backend/main.go
@@ -20,6 +20,7 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins: []string{
 			"http://localhost:3000",
+			"https://poker-bar.vercel.app",
 		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
